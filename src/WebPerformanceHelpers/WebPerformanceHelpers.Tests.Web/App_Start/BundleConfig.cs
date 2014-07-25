5418e8f3a2fbb4400cdae6b6de3e497861151c6a
benembery <benembery@gmail.com>
2014-07-25 14:27:35 +0100

    Script bundles CDN fall back example

--- src/WebPerformanceHelpers/WebPerformanceHelpers.Tests.Web/App_Start/BundleConfig.cs
+++ src/WebPerformanceHelpers/WebPerformanceHelpers.Tests.Web/App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace WebPerformanceHelpers.Tests.Web.App_Start
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.UseCdn = true;
+
             bundles.Add(new StyleBundle("~/bundles/css").Include("~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/bundles/css/theme").Include("~/Content/preboot.css"));
