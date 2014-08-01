844b41ecf6261411a6bb61c793c543233dd683bb
benembery <benembery@gmail.com>
2014-08-01 09:52:18 +0100

    Ajax Examples

--- src/WebPerformanceHelpers/WebPerformanceHelpers.Tests.Web/Controllers/AjaxIncludeExamplesController.cs
+++ src/WebPerformanceHelpers/WebPerformanceHelpers.Tests.Web/Controllers/AjaxIncludeExamplesController.cs
@@ -6,16 +6,39 @@ namespace WebPerformanceHelpers.Tests.Web.Controllers
     [RoutePrefix("ajax-include-examples")]
     public class AjaxIncludeExamplesController : Controller
     {
+        [Route("proxy")]
+        public ActionResult Proxy(AjaxIncludeProxyRequest proxyRequest, string includes)
+        {
+            return new AjaxIncludeProxyResult(proxyRequest);
+        }
+
         [Route("replace")]
         public ActionResult Replace()
         {
             return PartialView();
         }
 
-        [Route("proxy")]
-        public ActionResult Proxy(AjaxIncludeProxyRequest proxyRequest, string includes)
+        [Route("before")]
+        public ActionResult Before()
         {
-            return new AjaxIncludeProxyResult(proxyRequest);
+            return PartialView();
+        }
+
+        [Route("after")]
+        public ActionResult After()
+        {
+            return PartialView();
+        }
+
+        [Route("append")]
+        public ActionResult Append()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Prepend()
+        {
+            return PartialView();
         }
     }
 }
